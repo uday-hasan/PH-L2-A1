a3e19005bfbf3830c8c4aa5ea810861509f70e89
udayhasan <udayhasan15@gmail.com>
2025-11-17 00:57:10 +0600

    End Assignment-1

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# PH-L2-A1
+### What are some differences between interfaces and types in TypeScript?
+
+- প্রিমিটিভ ডাটা স্ট্রাকচার কে **type** এর মাধ্যমে প্রকাশ করা যায়, **interface** এর মাধ্যমে করা যায় না।
+- টুপল কে **type** এর মাধ্যমে প্রকাশ করা যায়, **interface** এর মাধ্যমে করা যায় না।
+- **type** এর মাধ্যমে লিটারেল(ইউনিয়ন) প্রকাশ করা যায়, **interface** এর মাধ্যমে করা যায় না।.
+
+---
+
+### What is the use of the keyof keyword in TypeScript? Provide an example.
+
+<p>**keyof** কি-ওয়ার্ড দ্বারা type বা interface এর key কে লিটারেল(ইউনিয়ন) হিসাবে পাওয়া যায়</p>
+
+#### Example
+
+```ts
+interface User {
+  id: number;
+  name: string;
+}
+
+type UserKeys = keyof User;
+```
---
+++ solution.ts
@@ -0,0 +1,109 @@
+const formatValue = (
+  value: string | number | boolean
+): string | number | boolean => {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+};
+
+const getLength = (value: any[] | string): number => {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (typeof value === "object" && Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
+};
+
+class Person {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}`;
+  }
+}
+
+interface Problem4Props {
+  title: string;
+  rating: number;
+}
+const filterByRating = (value: Problem4Props[]): Problem4Props[] | [] => {
+  return value.filter((item) => item.rating >= 4 && item.rating <= 5);
+};
+
+interface Problem5Props {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+}
+const filterActiveUsers = (data: Problem5Props[]): Problem5Props[] | [] => {
+  return data.filter((item) => !!item.isActive);
+};
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+const printBookDetails = (data: Book) => {
+  console.log(
+    `Title: ${data.title}, Author: ${data.author}, Published: ${
+      data.author
+    }, Available: ${data.isAvailable ? "Yes" : "No"}`
+  );
+};
+
+const getUniqueValues = <T extends number | string>(arr1: T[], arr2: T[]) => {
+  let uniqueItems: T[] = [];
+  const isExist = (value: T, arr: T[]): boolean => {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === value) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (!isExist(arr1[i], uniqueItems)) {
+      uniqueItems.push(arr1[i]);
+    }
+  }
+
+  for (let i = 0; i < arr2.length; i++) {
+    if (!isExist(arr2[i], uniqueItems)) {
+      uniqueItems.push(arr2[i]);
+    }
+  }
+
+  return uniqueItems;
+};
+
+interface Problem8Props {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+}
+const calculateTotalPrice = (products: Problem8Props[]): number => {
+  const mappedProducts = products.map((item) => {
+    const basePrice = item.price * item.quantity;
+    const discount = item.discount ? (basePrice * item.discount) / 100 : 0;
+    return basePrice - discount;
+  });
+
+  const totalPrice = mappedProducts.reduce((sum, acc) => sum + acc, 0);
+  return totalPrice;
+};
